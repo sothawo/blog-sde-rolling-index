8bb1eb60c4f312954fe38e5ee4f00d1f52669663
Peter-Josef Meisch <pj.meisch@sothawo.com>
2020-11-16 18:44:37 +0100

    initial commit

---
+++ src/main/java/com/sothawo/blogsderollingindex/BlogSdeRollingIndexApplication.java
@@ -0,0 +1,13 @@
+package com.sothawo.blogsderollingindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSdeRollingIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSdeRollingIndexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sothawo/blogsderollingindex/BlogSdeRollingIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sothawo.blogsderollingindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSdeRollingIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
